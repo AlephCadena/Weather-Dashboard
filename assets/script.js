a0dee023fbf3e29179064a6bbc3df081535fd60a
Aleph Cadena <alephcadena@gmail.com>
2021-10-24 23:58:19 -0500

    updated

--- assets/script.js
+++ assets/script.js
@@ -1,43 +1,44 @@
-var cityNameInputForm = $('#input-city-name');
+var cityInput = $('#input-city-name');
 var cityInfoEl = $('#city-card');
-//adding variables defining each aspect I want pulled up
-var currentCity = document.createElement('h2');
-var currentTemp = document.createElement('p');
-var currentWind = document.createElement('p');
-var currentHumidity = document.createElement('p');
-var currentUVIndexP = document.createElement('p')
-var currentUVIndex = document.createElement('span');
-var futureDate = document.createElement('h6')
-var icon = document.createElement('img')
-var futureTemp = document.createElement('p');
-var futureWind = document.createElement('p');
-var futureHumidity = document.createElement('p');
-var savedBtns  = document.createElement('button');    
 
+var cCity = document.createElement('h2');
+var cTemp = document.createElement('a');
+var cWind = document.createElement('a');
+var cHumidity = document.createElement('a');
+var cUVIndexP = document.createElement('a')
+var cUVIndex = document.createElement('span');
+
+var fDate = document.createElement('h6')
+var fTemp = document.createElement('a');
+var fWind = document.createElement('a');
+var fHumidity = document.createElement('a');
+
+var icon = document.createElement('img')
+var Btns  = document.createElement('button');    
 var today = moment();
-var savedCities;
+var savedLocations;
 
 function init() {
-    savedCities = JSON.parse(localStorage.getItem('savedCities')) || [];
+    savedLocations = JSON.parse(localStorage.getItem('savedLocations'));
     renderSearchHistoryBtn();
 };
 
 function handleSubmitFormSearch(event) {
     event.preventDefault();
-    var cityName = cityNameInputForm.val().trim();
-    if (cityName) {
-        getCityWeather(cityName);
+    var name = cityInput.val().trim();
+    if (name) {
+        getCityWeather(name);
     } else {
-        alert('Please enter a city name') //alert set in case no city is entered
+        alert('Please enter the name of a city')
     };
-    cityNameInputForm.val('');
+    cityInput.val('');
 };
 
 function handleSavedBtn(event) {
-    cityName = event.target.textContent;
-    getCityWeather(cityName);
+    name = event.target.textContent;
+    getCityWeather(name);
 };
-// added the function for the weather api to pull the info from their api for the city in question
+
 function getCityWeather(city) {
     var currentUrl = 'https://api.openweathermap.org/data/2.5/weather?q='+ city +'&units=imperial&appid=4ff77886755c8b9237b9bb4bb1c1e2bf'
     $.ajax({
@@ -45,7 +46,7 @@ function getCityWeather(city) {
         method: 'GET',
         statusCode: {
             404: function() {
-              alert( "City name not found. Please enter a valid city." ); //second alert in case a city that is not recognized is entered
+              alert( "City was not found, please enter a valid city name." ); 
             }
           }
     }).then(function (response) {
@@ -57,15 +58,15 @@ function getCityWeather(city) {
 
 function renderCityWeather(data) {  
     today = moment();
-    currentCity.textContent = data.name + ' (' + today.format("L") +') ';
-    currentTemp.textContent = 'Temp: ' + data.main.temp +'°F';
-    currentWind.textContent = 'Wind: ' + data.wind.speed + 'MPH';
-    currentHumidity.textContent = 'Humidity: ' + data.main.humidity + '%';
+    cCity.textContent = data.name + ' (' + today.format("L") +') ';
+    cTemp.textContent = 'Temperature: ' + data.main.temp +'°F';
+    cWind.textContent = 'Wind: ' + data.wind.speed + 'MPH';
+    cHumidity.textContent = 'Humidity: ' + data.main.humidity + '%';
 
-    cityInfoEl.append(currentCity);
-    cityInfoEl.append(currentTemp);
-    cityInfoEl.append(currentWind);
-    cityInfoEl.append(currentHumidity);
+    cityInfoEl.append(cCity);
+    cityInfoEl.append(cTemp);
+    cityInfoEl.append(cWind);
+    cityInfoEl.append(cHumidity);
     cityInfoEl.addClass('current-city');
     
     saveSearch(data.name);
@@ -83,19 +84,19 @@ function getForecast(lat,lon) {
 };
 
 function renderForecast(data) {
-    currentUVIndexP.textContent = '';
-    currentUVIndex.textContent = data.current.uvi;
-    currentUVIndexP.textContent = 'UV Index: ';
+    cUVIndexP.textContent = '';
+    cUVIndex.textContent = data.current.uvi;
+    cUVIndexP.textContent = 'UV Index: ';
     if (data.current.uvi <=2) {
-        currentUVIndex.classList.add('low');
+        cUVIndex.classList.add('low');
     } else if (data.current.uvi >=6) {
-        currentUVIndex.classList.add('high');
+        cUVIndex.classList.add('high');
     } else {
-        currentUVIndex.classList.add('moderate')
+        cUVIndex.classList.add('moderate')
     };
 
-    currentUVIndexP.append(currentUVIndex);
-    cityInfoEl.append(currentUVIndexP);
+    cUVIndexP.append(cUVIndex);
+    cityInfoEl.append(cUVIndexP);
     
     $('#5-day-forecast').text('5-Day-Forecast');
     for (var i=1; i<6; i++) {
@@ -104,25 +105,27 @@ function renderForecast(data) {
     
     for (var j=1; j<6; j++) {
 
-        futureDate = document.createElement('h6')
+        fDate = document.createElement('h6')
         icon = document.createElement('img')
-        futureTemp = document.createElement('p');
-        futureWind = document.createElement('p');
-        futureHumidity = document.createElement('p');
+        fTemp = document.createElement('a');
+        fWind = document.createElement('a');
+        fHumidity = document.createElement('a');
+        
         var iconCode = data.daily[j].weather[0].icon;
         var iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
         icon.setAttribute('src',iconUrl)
+
         today = moment();
-        futureDate.textContent = today.add(j, 'days').format('L');
-        futureTemp.textContent = 'Temp: ' +  data.daily[j].temp.day +'°F';
-        futureWind.textContent =  'Wind: ' + data.daily[j].wind_speed + 'MPH';
-        futureHumidity.textContent = 'Humidity: ' + data.daily[j].humidity + '%';
+        fDate.textContent = today.add(j, 'days').format('L');
+        fTemp.textContent = 'Temp: ' +  data.daily[j].temp.day +'°F';
+        fWind.textContent =  'Wind: ' + data.daily[j].wind_speed + 'MPH';
+        fHumidity.textContent = 'Humidity: ' + data.daily[j].humidity + '%';
         
-        $('#day-'+j).append(futureDate);
+        $('#day-'+j).append(fDate);
         $('#day-'+j).append(icon);
-        $('#day-'+j).append(futureTemp);
-        $('#day-'+j).append(futureWind);
-        $('#day-'+j).append(futureHumidity);
+        $('#day-'+j).append(fTemp);
+        $('#day-'+j).append(fWind);
+        $('#day-'+j).append(fHumidity);
         $('#day-'+j).addClass('p-3');
         $('#day-'+j).css({'min-height':'320px', 'width':'140px'});
     };
@@ -130,26 +133,26 @@ function renderForecast(data) {
 };
 
 function saveSearch(city) {
-    if (savedCities.includes(city)) {
+    if (savedLocations.includes(city)) {
         renderSearchHistoryBtn();
         return;
     };
-    savedCities.push(city)
-    localStorage.setItem('savedCities',JSON.stringify(savedCities));
+    savedLocations.push(city)
+    localStorage.setItem('savedLocations',JSON.stringify(savedLocations));
     renderSearchHistoryBtn();
 };
 
 function renderSearchHistoryBtn() {
     
     $('#saved-btn-div').children().remove('button');
-    for (var i=0; i<savedCities.length; i++) {
-        savedBtns = document.createElement('button');
-        savedBtns.setAttribute('class', 'saved-btn btn btn-secondary w-100');
-        savedBtns.textContent = savedCities[i];
-        $('#saved-btn-div').append(savedBtns);
+    for (var i=0; i<savedLocations.length; i++) {
+        Btns = document.createElement('button');
+        Btns.setAttribute('class', 'saved-btn btn btn-secondary w-100');
+        Btns.textContent = savedLocations[i];
+        $('#saved-btn-div').append(Btns);
     };
-    if (savedCities.length>3){
-        savedCities.shift();
+    if (savedLocations.length>3){
+        savedLocations.shift();
     };
     
 };
